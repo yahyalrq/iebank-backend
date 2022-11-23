150ecba2801628816041af3d2485d97bcb9c459d
yahyalrq <84201630+yahyalrq@users.noreply.github.com>
2022-11-23 17:14:46 +0100

    diozfad

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -1,3 +1,5 @@
+from iebank_api import routes
+from iebank_api.models import Account
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -7,6 +9,3 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Pa$$w0rd1990@localhost/iebank'
 db = SQLAlchemy(app)
-
-from iebank_api.models import Account
-from iebank_api import routes
