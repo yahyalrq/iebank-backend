e8adfce1352c31155ec7f44e2b9f9c76a9cc825b
yahyalrq <84201630+yahyalrq@users.noreply.github.com>
2022-11-23 17:07:06 +0100

    chafnging live session

--- config.py
+++ config.py
@@ -3,21 +3,25 @@ import os
 
 load_dotenv()
 
+
 class Config(object):
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = 'e0b9cada-61bc-4b5a-bd7a-52c606726b3b'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
+
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.getenv('DBUSER'),
-    dbpass=os.getenv('DBPASS'),
-    dbhost=os.getenv('DBHOST') + ".postgres.database.azure.com",
-    dbname=os.getenv('DBNAME')
+        dbuser=os.getenv('DBUSER'),
+        dbpass=os.getenv('DBPASS'),
+        dbhost=os.getenv('DBHOST') + ".postgres.database.azure.com",
+        dbname=os.getenv('DBNAME')
     )
+
+
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.getenv('DBUSER'),
-    dbpass=os.getenv('DBPASS'),
-    dbhost=os.getenv('DBHOST'),
-    dbname=os.getenv('DBNAME')
+        dbuser=os.getenv('DBUSER'),
+        dbpass=os.getenv('DBPASS'),
+        dbhost=os.getenv('DBHOST'),
+        dbname=os.getenv('DBNAME')
     )
