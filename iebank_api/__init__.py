08c2eebaf5379bb4bdf640a6752c045b2c4b0a3c
yahyalrq <84201630+yahyalrq@users.noreply.github.com>
2022-12-09 08:53:07 +0100

    update for deploy in dev

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -1,5 +1,3 @@
-from iebank_api import routes
-from iebank_api.models import Account
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -7,7 +5,6 @@ from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
-
 load_dotenv()
 
 # Select environment based on the ENV environment variable
@@ -20,6 +17,10 @@ elif os.getenv('ENV') == 'ghci':
 else:
     print("Running in production mode")
     app.config.from_object('config.ProductionConfig')
-
-
 db = SQLAlchemy(app)
+
+from iebank_api.models import Account
+db.create_all()
+CORS(app)
+
+from iebank_api import routes
